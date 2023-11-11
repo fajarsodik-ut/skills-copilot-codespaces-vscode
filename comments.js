db86db030ff9ccec98595cf9b301ce4070a2d3c8
Fajar Sodik <139830087+fajarsodik-ut@users.noreply.github.com>
2023-11-11 07:30:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,10 +1,21 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    // Handle incoming requests here
+// Create web server using express
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+// Create web server using express
+const app = express();
+// Add middleware to parse JSON
+app.use(bodyParser.json());
+// Add middleware to allow other domains to access our resources
+app.use(cors());
+// Add middleware for logging
+app.use(morgan('combined'));
+// Add a route handler
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'hello world!'
+    });
 });
-
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+// Start the server
+app.listen(process.env.PORT || 8081);
